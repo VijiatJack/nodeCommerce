52d970d1e7b0798787034a914a0f0ad77d1a3bbe
Jackson Silva <jackson.victorius@gmail.com>
2020-07-26 18:05:38 -0300

    keep going

---
+++ frontend/src/data.js
@@ -0,0 +1,58 @@
+export default {
+    products: [
+        {
+            name: "Slim Shirt",
+            category: "Shirts",
+            image: "/images/d1.jpg",
+            price: 60,
+            brand: "Nike",
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            name: "Fit Shirt",
+            category: "Shirts",
+            image: "/images/d1.jpg",
+            price: 23,
+            brand: "Nike",
+            rating: 3.2,
+            numReviews: 4
+        },
+        {
+            name: "Best Pants",
+            category: "Pants",
+            image: "/images/d1.jpg",
+            price: 65,
+            brand: "Nike",
+            rating: 5,
+            numReviews: 12
+        },
+        {
+            name: "Best Pants",
+            category: "Pants",
+            image: "/images/d1.jpg",
+            price: 65,
+            brand: "Nike",
+            rating: 5,
+            numReviews: 12
+        },
+        {
+            name: "Best Pants",
+            category: "Pants",
+            image: "/images/d1.jpg",
+            price: 65,
+            brand: "Nike",
+            rating: 5,
+            numReviews: 12
+        },
+        {
+            name: "Best Pants",
+            category: "Pants",
+            image: "/images/d1.jpg",
+            price: 65,
+            brand: "Nike",
+            rating: 5,
+            numReviews: 12
+        },
+    ]
+}
